fc9b4a2505e3b5eec9802ee088b8eb9c5b445e1f
Reinhard Holzner <reinhard@smint.io>
2022-01-12 09:30:54 +0100

    Make further classes public

--- Src/Newtonsoft.Json/Linq/JsonPath/QueryExpression.cs
+++ Src/Newtonsoft.Json/Linq/JsonPath/QueryExpression.cs
@@ -13,7 +13,7 @@ using Newtonsoft.Json.Utilities;
 
 namespace Newtonsoft.Json.Linq.JsonPath
 {
-    internal enum QueryOperator
+    public enum QueryOperator
     {
         None = 0,
         Equals = 1,
@@ -30,9 +30,9 @@ namespace Newtonsoft.Json.Linq.JsonPath
         StrictNotEquals = 12
     }
 
-    internal abstract class QueryExpression
+    public abstract class QueryExpression
     {
-        internal QueryOperator Operator;
+        public QueryOperator Operator { get; set; }
 
         public QueryExpression(QueryOperator @operator)
         {
@@ -42,7 +42,7 @@ namespace Newtonsoft.Json.Linq.JsonPath
         public abstract bool IsMatch(JToken root, JToken t);
     }
 
-    internal class CompositeExpression : QueryExpression
+    public class CompositeExpression : QueryExpression
     {
         public List<QueryExpression> Expressions { get; set; }
 
@@ -79,7 +79,7 @@ namespace Newtonsoft.Json.Linq.JsonPath
         }
     }
 
-    internal class BooleanQueryExpression : QueryExpression
+    public class BooleanQueryExpression : QueryExpression
     {
         public readonly object Left;
         public readonly object? Right;
